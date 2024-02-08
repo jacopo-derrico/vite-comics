4b9e64af32123f91f00670e5bd9555f7829226f4
jacopo-derrico <derricoj.design@gmail.com>
2024-02-08 17:10:17 +0100

    add comics img hover

--- src/components/DCSlides.vue
+++ src/components/DCSlides.vue
@@ -29,12 +29,18 @@ div {
     figure {
         width: 100%;
         margin-bottom: 20px;
+        overflow: hidden;
 
         img {
             width: 100%;
             display: block;
             object-fit: cover;
             aspect-ratio: 1;
+        }
+
+        &:hover img {
+            scale: 1.3;
+            transition: scale 0.5s ease-in-out;
         }
     }
 }
